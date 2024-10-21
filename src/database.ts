9605aaad66c13b73789a740920a8451f68e57215
Yury Staravoitau <Morfinall@mail.ru>
2024-10-21 07:38:38 +0300

    fix: change folder for db

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 import { join } from 'path';
 
-const usersFilePath = join(__dirname, 'db', 'users.json');
+const usersFilePath = join(__dirname, '..', 'db', 'users.json');
 
 interface User {
     id: string;
